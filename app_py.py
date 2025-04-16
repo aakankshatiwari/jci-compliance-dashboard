6183b5d31cbb471bfc405450951bd9505507e511
Aakanksha Tiwari <akki2tiwari@gmail.com>
2025-04-16 19:40:11 +0530

    Update app_py.py

--- app_py.py
+++ app_py.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/#fileId=https%3A//storage.googleapis.com/kaggle-colab-exported-notebooks/aakanksha54/app-py.9d855850-ea2d-4e8b-9063-7d02d4b09c46.ipynb%3FX-Goog-Algorithm%3DGOOG4-RSA-SHA256%26X-Goog-Credential%3Dgcp-kaggle-com%2540kaggle-161607.iam.gserviceaccount.com/20250416/auto/storage/goog4_request%26X-Goog-Date%3D20250416T140128Z%26X-Goog-Expires%3D259200%26X-Goog-SignedHeaders%3Dhost%26X-Goog-Signature%3D50a2f42a7205cb782af25f8d789f53aa35894f1bd7147352abe5b25fcaaf9e97b7f497972466be91ed34625a96f3d4dd1f541df5a3f6439b094a596be5b421dd7edec4516d126a87de398efa35454a64b5638ef9287531a648188e41c019283f93b2289b6b2969ecd68ea4a941a5e675ce9c93ba39c4fbb022998a1d17b52f3a9baf9daebf1cd21d0949965aba912a1811143fda70d4e0f883ba7cc2013f784932ff1683275cc37be9ee8c2cf8120e630f99a69b148ed5f045cb64812a5086121efbab4c39223eca3a4d6b0acc0bfc028b0787382adf4303eeee5a7c99c0311ee5cf957081549711e4428f397d0d893c36788b9f56886bab9c0ef94c1cc6f2fe
 """
 
-!pip install -q streamlit fpdf
 
 import streamlit as st
 import pandas as pd
